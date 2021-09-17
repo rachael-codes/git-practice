5c5eb98043b50d8bb5c3c2265b257f02a156b910
Edmine <edmine.de@gmail.com>
2021-09-17 16:43:41 -0400

    add function c

--- function_c.py
+++ function_c.py
@@ -1,9 +1,9 @@
 def merge_lists(list_a, list_b):
     """ Returns a new list which is
         a combination of list_a and list_b
-        without any duplicate elements.
-    """
-    pass
+        without any duplicate elements."""
+
+    return list(set(list_a) | set(list_b))   
 
 
 if __name__ == "__main__":

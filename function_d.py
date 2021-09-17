20788a445e6ef98d23c865a134b1306f6951eb8b
Vange Spracklin <houseofvange@gmail.com>
2021-09-17 13:51:05 -0700

    here is my function d version

--- function_d.py
+++ function_d.py
@@ -2,8 +2,10 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
-
+    print("hello git ready to party!")
+    max_value = max(numbers)
+    
+    return max_value
 
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
